93d08c48f1abe1ea3551c5913118f3eeadf5a965
chiwon son <sonchiwon91@gmail.com>
2024-07-19 17:09:22 +0900

    디렉토리 구축중

--- BreadTour/src/main/java/com/example/BreadTour/BreadTourApplication.java
+++ BreadTour/src/main/java/com/example/BreadTour/BreadTourApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.BreadTour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BreadTourApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BreadTourApplication.class, args);
 	}
 
 }
