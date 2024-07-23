6695e74b9ef0bf127e87ed8122b8458108e0c2e1
chiwon son <sonchiwon91@gmail.com>
2024-07-23 09:22:50 +0900

    디렉토리 수정중

--- BreadTour/src/main/java/BreadTour/controllers/MyController.java
+++ BreadTour/src/main/java/BreadTour/controllers/MyController.java
@@ -1,15 +1,16 @@
 package BreadTour.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ViewController {
+@RequestMapping("/api")
+public class MyController {
 
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, BreadTour!";
+    public String hello() {
+        return "Hello, World!";
     }
-
 }
 // http://localhost:8081/hello
--- BreadTour/src/main/java/BreadTour/models/Member/User.java
+++ BreadTour/src/main/java/BreadTour/models/Member/User.java
@@ -1,5 +1,18 @@
 package BreadTour.models.Member;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    // getters and setters
 }
