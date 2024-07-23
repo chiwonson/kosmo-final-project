6f365a98462a9e9b3d3592ef44c9973abc47c543
kyugwon <irin1015@naver.com>
2024-07-23 15:24:52 +0900

    240723

---
+++ BreadTour/src/main/java/BreadTour/models/Product/vo/ProductVO.java
@@ -0,0 +1,15 @@
+package BreadTour.models.Product.vo;
+
+public class ProductVO {
+
+    private String crnum;
+    private String crid;
+    private String mid;
+    private String mname;
+    private String crcnt;
+    private String crtotal;
+    private String prphoto;
+    private String insertdate;
+    private String prid;
+
+}
