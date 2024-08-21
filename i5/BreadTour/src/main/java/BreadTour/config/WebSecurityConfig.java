a4b4f3f62c6ab3f2a2111dbe400e2c827f2befdd
chiwonson <166478273+chiwonson@users.noreply.github.com>
2024-08-21 11:25:43 +0900

    i5

--- i5/BreadTour/src/main/java/BreadTour/config/WebSecurityConfig.java
+++ i5/BreadTour/src/main/java/BreadTour/config/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig implements WebMvcConfigurer {
                 http
                                 .authorizeHttpRequests(authorize -> authorize
                                                 .requestMatchers("/login", "/logout", "/welcome", "/signup",
-                                                                "/user", "/main", "/check-email", "/api/**",
+                                                                "/user", "/main", "/user/check-email", "/api/**",
                                                                 "/order-success")
                                                 .permitAll()
                                                 .requestMatchers("/shopping", "/edit", "/reservation",
